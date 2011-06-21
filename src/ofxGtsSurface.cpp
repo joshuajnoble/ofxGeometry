d5c368ae1ae155317258a0dbd40689623e9309e6
base <base@base.home>
2011-06-21 23:34:11 +0200

    just committing && pushing so i can move things around

--- src/ofxGtsSurface.cpp
+++ src/ofxGtsSurface.cpp
@@ -28,19 +28,109 @@ void ofxGtsSurface::createSphere(guint level) {
 }
 
 vector<GtsVertex*> ofxGtsSurface::getVertices() {
-	vector<GtsVertex*> vertices;
-	gts_surface_foreach_vertex(s, (GtsFunc) build_vertex_vector, &vertices);
-	return vertices;
+	//vector<GtsVertex*> vertices;
+	gts_surface_foreach_vertex(s, (GtsFunc) build_vertex_vector, &surfaceVertices);
+	return surfaceVertices;
 }
 
 vector<GtsEdge*> ofxGtsSurface::getEdges() {
-	vector<GtsEdge*> edges;
-	gts_surface_foreach_edge(s, (GtsFunc) build_edge_vector, &edges);
-	return edges;
+	//vector<GtsEdge*> edges;
+	gts_surface_foreach_edge(s, (GtsFunc) build_edge_vector, &surfaceEdges);
+	return surfaceEdges;
 }
 
-vector<GtsTriangle*> ofxGtsSurface::getTriangles() {
-	vector<GtsTriangle*> triangles;
-	gts_surface_foreach_face(s, (GtsFunc) build_triangle_vector, &triangles);
-	return triangles;
+vector<GtsTriangle*>& ofxGtsSurface::getTriangles() {
+	//vector<GtsTriangle*> triangles;
+	gts_surface_foreach_face(s, (GtsFunc) build_triangle_vector, &surfaceTriangles);
+	return surfaceTriangles;
+}
+
+void ofxGtsSurface::addVertices(vector<ofVec3f> verts) {
+	
+	
+	
+}
+
+
+bool ofxGtsSurface::intersects(ofxGtsSurface* surfaceToCheck) {
+	
+	return false;
+}
+
+void ofxGtsSurface::merge(ofxGtsSurface* surfaceToMerge) {
+	
+	gts_surface_merge (s, s1);
+	
+	// now delete the other surface?
+}
+
+void ofxGtsSurface::cleanupTriangles()
+{
+	
+	
+	GSList * triangles = NULL;
+	GSList * i;
+	
+	g_return_if_fail (s != NULL);
+	
+	/* build list of triangles */
+	gts_surface_foreach_face (s, (GtsFunc) build_list, &triangles);
+	
+	/* remove duplicate triangles */
+	i = triangles;
+	while (i) {
+		GtsTriangle * t = i->data;
+		if (gts_triangle_is_duplicate (t))
+		/* destroy t, its edges (if not used by any other triangle)
+		 and its corners (if not used by any other edge) */
+			gts_object_destroy (GTS_OBJECT (t));
+		i = i->next;
+	}
+	
+	/* free list of triangles */
+	g_slist_free (triangles);
+}
+
+void ofxGtsSurface::cleanupVertices()
+{
+	
+	GSList * edges = NULL;
+	GSList * i;
+	
+	g_return_if_fail (s != NULL);
+	
+	/* build list of edges */
+	gts_surface_foreach_edge (s, (GtsFunc) build_list, &edges);
+	
+	/* remove degenerate and duplicate edges.
+     Note: we could use gts_edges_merge() to remove the duplicates and then
+     remove the degenerate edges but it is more efficient to do everything 
+     at once (and it's more pedagogical too ...) */
+	
+	/* We want to control manually the destruction of edges */
+	gts_allow_floating_edges = TRUE;
+	
+	i = edges;
+	while (i) {
+		GtsEdge * e = i->data;
+		GtsEdge * duplicate;
+		if (GTS_SEGMENT (e)->v1 == GTS_SEGMENT (e)->v2) /* edge is degenerate */
+		/* destroy e */
+			gts_object_destroy (GTS_OBJECT (e));
+		else if ((duplicate = gts_edge_is_duplicate (e))) {
+			/* replace e with its duplicate */
+			gts_edge_replace (e, duplicate);
+			/* destroy e */
+			gts_object_destroy (GTS_OBJECT (e));
+		}
+		i = i->next;
+	}
+	
+	/* don't forget to reset to default */
+	gts_allow_floating_edges = FALSE;
+	
+	/* free list of edges */
+	g_slist_free (edges);
+	
+	
 }
--- src/ofxGtsSurface.h
+++ src/ofxGtsSurface.h
@@ -8,10 +8,24 @@ class ofxGtsSurface {
 public: 
 	ofxGtsSurface();
 	void createSphere(guint level);
-	vector<GtsVertex*> getVertices();
-	vector<GtsEdge*> getEdges();
-	vector<GtsTriangle*> getTriangles();
+	vector<GtsVertex*>& getVertices();
+	vector<GtsEdge*>& getEdges();
+	vector<GtsTriangle*>& getTriangles();
+	
+	void ofxGtsSurface::addVertices(vector<ofVec3f> verts);
+	bool ofxGtsSurface::intersects(ofxGtsSurface* surfaceToCheck);
+	void ofxGtsSurface::merge(ofxGtsSurface* surfaceToMerge);
+	
+	void ofxGtsSurface::cleanupTriangles();
+	
+	void ofxGtsSurface::cleanupVertices();
+	
+	
 private:
 	GtsSurface* s;
+	
+	vector<GtsTriangle*> surfaceTriangles;
+	vector<GtsVertex*> surfaceVertexes;
+	vector<GtsEdge*> surfaceEdges;
 	
 };
